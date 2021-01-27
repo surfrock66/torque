c7e21db1f4eab85f0fc69e751ab08c4167a53e5e
Esprit1st <39107090+Esprit1st@users.noreply.github.com>
2021-01-26 20:45:11 -0700

    Create torquehelpers2.js
    Moving functions that need to be loaded after the document has been rendered into an extra torquehelpers file.
    Adding new function for changing export.php link when a selection on the graph has been made.

---
+++ web/static/js/torquehelpers2.js
@@ -0,0 +1,20 @@
+// change timestamps to readable date in selection dropdown
+$('#seshidtag option').each(function() {
+	entrydate = $(this).text().substr(0, 13);
+	if (ld = parseInt(entrydate)) {
+		ld = new Date( ld );
+		ld = ld.toString().substr(4,17);
+		this.text = ld + $(this).text().substr(13);
+	}
+});
+
+// change url for export when graph has selection
+var placeholder = $("#placeholder");
+placeholder.bind("plotselected", function (event, ranges) {
+	document.getElementById("export").children[0].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=csv&from=" + ranges.xaxis.from.toFixed(1).substring(0,13) + "&to=" + ranges.xaxis.to.toFixed(1).substring(0,13);
+	document.getElementById("export").children[1].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=json&from=" + ranges.xaxis.from.toFixed(1).substring(0,13) + "&to=" + ranges.xaxis.to.toFixed(1).substring(0,13);
+});
+placeholder.bind("plotunselected", function (event) {
+	document.getElementById("export").children[0].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=csv";
+	document.getElementById("export").children[1].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=json";
+});
