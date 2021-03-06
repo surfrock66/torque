630eefa66bbe88bf5cc059ba9d97a977112dd84c
Esprit1st <39107090+Esprit1st@users.noreply.github.com>
2021-03-05 17:29:35 -0700

    Delete range
    added delete the range that is selected in the graph

--- web/del_session.php
+++ web/del_session.php
@@ -6,17 +6,23 @@ if (!isset($_SESSION)) { session_start(); }
 
 if (isset($_POST["deletesession"])) {
     $deletesession = preg_replace('/\D/', '', $_POST['deletesession']);
+	$deletefrom = preg_replace('/\D/', '', $_POST['from']);
+	$deleteto = preg_replace('/\D/', '', $_POST['to']);
 }
 elseif (isset($_GET["deletesession"])) {
     $deletesession = preg_replace('/\D/', '', $_GET['deletesession']);
+	$deletefrom = preg_replace('/\D/', '', $_GET['from']);
+	$deleteto = preg_replace('/\D/', '', $_GET['to']);
 }
 
 if (isset($deletesession) && !empty($deletesession)) {
     $tableYear = date( "Y", $deletesession/1000 );
     $tableMonth = date( "m", $deletesession/1000 );
     $db_table_full = "{$db_table}_{$tableYear}_{$tableMonth}";
-    $delresult = mysqli_query($con, "DELETE FROM $db_table_full WHERE session=".quote_value($deletesession)) or die(mysqli_error($con));
-    $delresult = mysqli_query($con, "DELETE FROM $db_sessions_table WHERE session=".quote_value($deletesession)) or die(mysqli_error($con));
+    if (isset($deletefrom) && !empty($deletefrom) && isset($deleteto) && !empty($deleteto)) $range = " AND time >= " . quote_value($deletefrom) . " AND time <= " . quote_value($deleteto);
+	else $range = "";
+	$delresult = mysqli_query($con, "DELETE FROM $db_table_full WHERE session=".quote_value($deletesession).$range) or die(mysqli_error($con));
+    if (empty($range)) $delresult = mysqli_query($con, "DELETE FROM $db_sessions_table WHERE session=".quote_value($deletesession)) or die(mysqli_error($con));
 }
 //echo "<!-- End del_session.php at ".date("H:i:s", microtime(true))." -->\r\n";
 ?>
--- web/static/js/torquehelpers2.js
+++ web/static/js/torquehelpers2.js
@@ -13,8 +13,10 @@ var placeholder = $("#placeholder");
 placeholder.bind("plotselected", function (event, ranges) {
 	document.getElementById("export").children[0].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=csv&from=" + ranges.xaxis.from.toFixed(1).substring(0,13) + "&to=" + ranges.xaxis.to.toFixed(1).substring(0,13);
 	document.getElementById("export").children[1].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=json&from=" + ranges.xaxis.from.toFixed(1).substring(0,13) + "&to=" + ranges.xaxis.to.toFixed(1).substring(0,13);
+	$("#formdelete").attr('action', "session.php?deletesession=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&from=" + ranges.xaxis.from.toFixed(1).substring(0,13) + "&to=" + ranges.xaxis.to.toFixed(1).substring(0,13));
 });
 placeholder.bind("plotunselected", function (event) {
 	document.getElementById("export").children[0].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=csv";
 	document.getElementById("export").children[1].href = "export.php?sid=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value + "&filetype=json";
+	$("#formdelete").attr('action', "session.php?deletesession=" + document.getElementById("seshidtag")[document.getElementById("seshidtag").selectedIndex].value);
 });
